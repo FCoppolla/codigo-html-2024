64f74de43e3eb9bb9e9a21d98b4cd18cc8c7874e
FCoppolla <felipe.v.coppolla@gmail.com>
2024-11-04 21:56:24 -0300

    atualização aula 5
    atualização

---
+++ front_end/aula5/exemplo2/exemplo2.js
@@ -0,0 +1,17 @@
+document.write("ola terra !")
+alert('olá')
+console.log('olá, tudo bem?')
+var my_var;
+my_var = 'ola, pessoal';
+alert(my_var)
+my_var = 5 ;
+alert (my_var)
+my_var = [1, 10 , 'olá', true];
+alert(my_var)
+
+
+
+
+document.write('ola mundo !');
+document.write('<h1>eu sou um titulo </h1>');
+document.write('<title>titulo da pagina </title>');
